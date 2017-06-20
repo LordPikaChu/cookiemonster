0dbe5bf1b4193c7711d60cb51037c5bac488691a
Mateusz Hajdziony <mateusz.hajdziony@gmail.com>
2017-06-21 01:59:24 +0200

    Basic sample bot test.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('ITS WORKING');
+import Botkit from 'botkit';
+
+import slackConfig from '../slack.config.json';
+
+console.log(Botkit);
+
+var controller = Botkit.slackbot({debug: false})
+controller
+  .spawn({
+    token: slackConfig['bot_user_access_token']
+  })
+  .startRTM(function (err) {
+    if (err) {
+      throw new Error(err)
+    }
+  })
+
+controller.hears(
+  ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
+  function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
