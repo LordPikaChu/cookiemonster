b55357eb975c57c41fc4be41de0e961a7e6d0a7d
Mateusz Hajdziony <mateusz.hajdziony@gmail.com>
2017-06-21 11:54:11 +0200

    Added sample config file.

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+import SLACK_CONFIG from './slack.config.json';
+
 export default {
-  BOT_AUTH_TOKEN: process.env.BOT_AUTH_TOKEN
+  BOT_AUTH_TOKEN: process.env.BOT_AUTH_TOKEN || SLACK_CONFIG['bot_user_access_token']
 }
--- server/index.js
+++ server/index.js
@@ -2,17 +2,16 @@ import Botkit from 'botkit';
 
 import CONFIG from '../config';
 
-var controller = Botkit.slackbot({debug: false})
-controller
-  .spawn({
-    token: CONFIG.BOT_AUTH_TOKEN
-  })
-  .startRTM(function (err) {
-    if (err) {
-      throw new Error(err)
-    }
-  })
+var controller = Botkit.slackbot({debug: false});
 
-controller.hears(
-  ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-  function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
+controller
+.spawn({token: CONFIG.BOT_AUTH_TOKEN})
+.startRTM(function (err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+controller.on('reaction_added', function(bot, reaction) {
+  console.log(reaction);
+});
